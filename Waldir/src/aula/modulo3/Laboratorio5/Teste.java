39c353be289cf6691b421a9c5460b6bd7b3df2a9
unknown <meloferreiragabriel17@gmail.com>
2021-05-11 15:57:09 -0300

    Laboratorio 5

---
+++ Waldir/src/aula/modulo3/Laboratorio5/Teste.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class Teste {
+    
+    DaoAluno criAluno = new DaoAluno();
+	@Test
+	void update() {
+		var aluno = new Aluno();
+		assertEquals(criAluno.update(aluno), true);
+		
+	}
+	
+	@Test
+	void save() {
+		var aluno = new Aluno();
+		criAluno.update(aluno);
+		assertEquals(criAluno.save(aluno), true);
+	}
+	@Test
+	void delete() {
+		var aluno = new Aluno();
+		criAluno.update(aluno);
+		assertEquals(criAluno.delete(aluno), true);
+	}
+}
